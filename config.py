2c00962cc0663e5ec6ac7da4a31a5ee8a062f5c2
awave1 <artgolovin96@gmail.com>
2020-05-04 19:50:55 -0600

    🗑️  Cleaned: remove .vscode folder from source control

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
 
 HOST = "0.0.0.0"
-PORT = os.environ.get('PORT', 8443)
+PORT = os.environ.get("PORT", 8443)
 
-token = os.getenv('MUSIC_BOT_TOKEN')
+token = os.getenv("MUSIC_BOT_TOKEN")
 
 heroku_webhook = "https://find-music-links.herokuapp.com/bot"
 
-welcome_message = "Hello!\n" \
-                  "I can help you to share music with your friends in their music apps.\n" \
-                  "Enter link to spotify or yandex music and i'll send you link to another services "
+welcome_message = (
+    "Hello!\n"
+    "I can help you to share music with your friends in their music apps.\n"
+    "Enter link to spotify or yandex music and i'll send you link to another services "
+)
